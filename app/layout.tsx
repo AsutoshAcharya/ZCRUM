cb5a8bd3f99ab8ea9f217aaaf4d0a02a798580a8
Asutosh Acharya <asutosh246@gmail.com>
2025-06-12 23:59:08 +0530

    added basci themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -13,19 +16,34 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className}`}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <Header />
-          <main className="min-h-screen">{children}</main>
+    <ClerkProvider
+      appearance={{
+        baseTheme: shadesOfPurple,
+        variables: {
+          colorPrimary: "#3b82f6",
+          colorBackground: "#1a202c",
+          colorInputBackground: "#2D3748",
+          colorInputText: "#F3F4F6",
+        },
+        elements: {
+          formButtonPrimary: "bg-red-200",
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className}`}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <Header />
+            <main className="min-h-screen">{children}</main>
 
-          <footer className="bg-gray-900 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>Made by Asutosh</p>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+            <footer className="bg-gray-900 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made by Asutosh</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
